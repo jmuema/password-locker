6935c7f882d27ec1b4f404ca6cf09093973023a8
josephmuema <jmuema95@gmail.com>
2019-05-10 11:23:04 +0300

    Created a code to help generate a password

--- user.py
+++ user.py
@@ -1,16 +1,18 @@
 import random
+
+
 class User:
     '''
     Class that generates new users
     '''
-    user_list=[]
+    user_list = []
 
-    def __init__(self,username,password):
+    def __init__(self, username, password):
         '''
         defining properties for our object
         '''
-        self.myusername=username
-        self.mypassword=password
+        self.myusername = username
+        self.mypassword = password
 
     def save_user(self):
         '''
@@ -18,3 +20,12 @@ class User:
         '''
 
         User.user_list.append(self)
+
+    def generate_password():
+        chars = "abcdefghijklmnopqrstuvwxyz/;'[]=)(*$@)"
+        password = ""
+        for i in range(10):
+            password += random.choice(chars)
+        return password  # computated functions give a return value
+
+    
