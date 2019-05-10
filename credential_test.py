4f0283f653d2a0bea9cce5b396b953ed9b93ee65
josephmuema <jmuema95@gmail.com>
2019-05-10 12:59:52 +0300

    Wrote code to check if one can find the credential

--- credential_test.py
+++ credential_test.py
@@ -71,7 +71,18 @@ class TestCredential(unittest.TestCase):
 
         self.assertEqual(found_credential.mypassword,test_credential.mypassword)
 
-    
+    def test_credential_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find the credential.
+        '''
+
+        self.new_credential.save_credential()
+        test_credential = Credential("twitter","batman","drknight") # new credential
+        test_credential.save_credential()
+
+        credential_exists = Credential.credential_exist("batman")
+
+        self.assertTrue(credential_exists)
 
 
 if __name__ == '__main__':
