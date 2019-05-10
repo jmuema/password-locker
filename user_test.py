bd4d8c59371dbf2947379004131f709eba9786cd
josephmuema <jmuema95@gmail.com>
2019-05-10 11:54:35 +0300

    Created code to remove a user from the contact list

--- user_test.py
+++ user_test.py
@@ -47,6 +47,18 @@ class TestUser(unittest.TestCase):
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
 
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our contact list
+        '''
+        self.new_user.save_user()
+        test_user = User("Hal","1234")
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)
+
+   
 
 if __name__ == '__main__':
     unittest.main()
