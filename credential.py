a360bdaa9064a37b867738041fd2edda1117aa7d
josephmuema <jmuema95@gmail.com>
2019-05-10 12:16:22 +0300

    Wrote code to save credentials

--- credential.py
+++ credential.py
@@ -7,4 +7,16 @@ class Credential:
     '''
     credential_list=[]
 
+    def __init__(self, accountname, username, password):
+        self.myaccountname = accountname
+        self.myusername = username
+        self.mypassword = password
+
+    def save_credential(self):
+        '''
+        save_credential method saves credential objects into credential_list
+        '''
+
+        # appends every new value in the credential list
+        Credential.credential_list.append(self)
    
