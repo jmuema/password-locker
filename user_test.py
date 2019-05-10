bc560ae27ce03e98c20dedb450afb81a6997557b
josephmuema <jmuema95@gmail.com>
2019-05-10 11:37:40 +0300

    Added the code that would enable one to save multiple users and ran the test

--- user_test.py
+++ user_test.py
@@ -30,5 +30,23 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
 
+     # setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+    
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_user to check if we can save multiple user
+            objects to our user_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Oliver","1234") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+
+
 if __name__ == '__main__':
     unittest.main()
